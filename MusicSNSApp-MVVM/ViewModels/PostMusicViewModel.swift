ba6055b99382df1cfff4a6027a7593a703698b0d
mikekondo <foad5969@gmail.com>
2022-10-06 15:27:23 +0900

    add: ViewModelの作成、Bind

--- MusicSNSApp-MVVM/ViewModels/PostMusicViewModel.swift
+++ MusicSNSApp-MVVM/ViewModels/PostMusicViewModel.swift
@@ -9,14 +9,43 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+// MARK: - Inputs
 protocol PostMusicViewModelInput {
-
+    var commentTextViewObservable: Observable<String> { get }
+    var postButtonTapObservable: Observable<Void> { get }
 }
 
+// MARK: - Outputs
 protocol PostMusicViewModelOutput {
-    
+    var isPostSuccess: PublishSubject<Bool> { get }
 }
 
-class PostMusicViewModel {
+class PostMusicViewModel: PostMusicViewModelInput,PostMusicViewModelOutput{
+    // MARK: Inputs
+    var commentTextViewObservable: RxSwift.Observable<String>
+    var postButtonTapObservable: RxSwift.Observable<Void>
+
+    // MARK: Outputs
+    var isPostSuccess = RxSwift.PublishSubject<Bool>()
+
+    private let disposeBag = DisposeBag()
+
+    private var comment = ""
+
+    init(commentTextViewObservable: Observable<String>,postButtonTapObservable: Observable<Void>){
+        self.commentTextViewObservable = commentTextViewObservable
+        self.postButtonTapObservable = postButtonTapObservable
+        setupBindings()
+    }
+
+    private func setupBindings() {
+        commentTextViewObservable.subscribe { comment in
+            self.comment = comment
+        }.disposed(by: disposeBag)
+
+        postButtonTapObservable.subscribe (onNext: { [weak self] in
+            print("tap")
+        }).disposed(by: disposeBag)
+    }
 
 }
--- MusicSNSApp-MVVM/Views/SearchMusic/PostMusic/PostMusicViewController.swift
+++ MusicSNSApp-MVVM/Views/SearchMusic/PostMusic/PostMusicViewController.swift
@@ -12,23 +12,34 @@ import PKHUD
 import SDWebImage
 
 class PostMusicViewController: UIViewController {
+    // MARK: - UI Parts
     @IBOutlet weak var artworkImageView: UIImageView!
     @IBOutlet weak var trackNameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var commentTextView: UITextView!
     @IBOutlet weak var postButton: UIButton!
 
+    // MARK: - Model Connect
     var selectedMusic: MusicInfo?
+
+    private lazy var postViewModel = PostMusicViewModel(commentTextViewObservable: commentTextView.rx.text.map{$0 ?? ""}.asObservable(), postButtonTapObservable: postButton.rx.tap.asObservable())
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        setupBindings()
     }
 
     private func setupView() {
         guard let selectedMusic = selectedMusic else { return }
         artworkImageView.sd_setImage(with: URL(string: selectedMusic.artworkUrl100))
         trackNameLabel.text = selectedMusic.trackName
-        artistNameLabel.text = artistNameLabel.text
+        artistNameLabel.text = selectedMusic.artistName
+    }
+
+    private func setupBindings() {
+        postViewModel.isPostSuccess
     }
 
 }
