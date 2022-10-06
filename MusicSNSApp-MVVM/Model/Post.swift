7fe798f7a600a523aa8554ad6ddf35016e9bf6aa
mikekondo <foad5969@gmail.com>
2022-10-06 15:33:00 +0900

    add: postモデルを作成

--- MusicSNSApp-MVVM/Model/Post.swift
+++ MusicSNSApp-MVVM/Model/Post.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import Firebase
+
+struct Post {
+    let artistName: String
+    let artworkUrl: String
+    let trackName: String
+    let comment: String
+    let createdAt: Timestamp
+
+    init(dic: [String: Any]) {
+        self.artistName = dic["artistName"] as? String ?? ""
+        self.trackName = dic["trackName"] as? String ?? ""
+        self.comment = dic["comment"] as? String ?? ""
+        self.artworkUrl = dic["artworkUrl"] as? String ?? ""
+        self.createdAt = dic["createdAt"] as? Timestamp ?? Timestamp()
+    }
+}
