c3aecd55ba607ad696fea0ad921078a383567481
mikekondo <foad5969@gmail.com>
2022-10-07 15:27:23 +0900

    add: 投稿データの一覧表示

--- MusicSNSApp-MVVM/Views/PostsList/PostsListViewController.swift
+++ MusicSNSApp-MVVM/Views/PostsList/PostsListViewController.swift
@@ -11,18 +11,28 @@ import RxCocoa
 
 class PostsListViewController: UIViewController {
 
+    // MARK: - UI Parts
     @IBOutlet weak var tableView: UITableView!
+
+    let postListViewModel = PostListViewModel()
 
     private let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupBindings()
+        setupTableView()
         navigationItem.title = "PostList"
     }
 
     private func setupTableView() {
         tableView.rowHeight = 550
         tableView.register(UINib(nibName: PostTableViewCell.nibName, bundle: nil), forCellReuseIdentifier: PostTableViewCell.identifier)
+    }
+
+    private func setupBindings() {
+        postListViewModel.fetchPostPublishSubject
+            .bind(to: tableView.rx.items(dataSource: PostTableViewDataSource()))
+            .disposed(by: disposeBag)
     }
 
 }
